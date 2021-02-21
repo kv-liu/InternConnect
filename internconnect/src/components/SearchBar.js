e017d24edbe0e077062fe79e321069a3a7da5054
Han Zhang <hanzhang5976@icloud.com>
2021-02-21 05:24:33 -0800

    routing and input

--- internconnect/src/components/SearchBar.js
+++ internconnect/src/components/SearchBar.js
@@ -1,14 +1,33 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {input: ''};
+    }
+
+    handleChange = event  => {
+        this.setState({input: event.target.value})
+    }
+
     render() {
         return (
-            <form className="search-form" action="">
-                <input type="search" className="search-bar" placeholder="Type to search..." required></input>
-                <i class="fa fa-search"></i>
-            </form>
-                
-            
+        <div className="search-div" action="">
+            <input type="search" className="search-bar" placeholder="Type to search..." required value={this.state.input} onChange={this.handleChange}></input>
+            <Link to='/results'>
+            <button type="submit" className="search-btn"
+            // onClick={getAPI}
+            />
+            </Link>
+            <i className="fa fa-search"></i>
+            <h1 className="test-input">Your input is: {this.state.input}</h1>
+        </div>
+
+
+
+
         );
     }
 }
